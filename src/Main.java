9c99e770dd2575f9f75d817a455d9e2f5c588b11
alamin5g <21303134@iubat.edu>
2024-02-04 04:07:35 +0600

    Show the Data from Database

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,38 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.mysql.jdbc.Driver;
+
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
         
-        String url = "jdbc:mysql://localhost:3306/Student";
+        String url = "jdbc:mysql://localhost:3306/school";
         String user = "root";
         String pass = "252646";
 
-        try(Connection connection = DriverManager.getConnection(url, user, pass)) {
-            System.out.println("Database connected Successfully!");
-        } catch (SQLException e) {
-            System.err.println("Connection Failed: " + e.getMessage());
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded successfully!");
+        }catch (ClassNotFoundException c){
+            System.err.println("com.mysql..cj.jdbc.Driver - not found/loaded successfully!-- " + c.getMessage());
+        }
+
+        try{
+            Connection connection = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connection established success with Database!");
+            Statement statement = connection.createStatement();
+            String query = "select * from student";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                String address = resultSet.getString(3);
+                System.out.println("The " + id + " id's Student name is " + name + " and his/her address is " + address);
+            }
+            statement.close();
+            resultSet.close();
+        }catch (SQLException s){
+            System.err.println("Database Connection Failed - " + s.getMessage());
         }
     }
 }
