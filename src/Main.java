e0c318d8682591652be302c411e165e0a9c095ad
alamin5g <21303134@iubat.edu>
2024-02-04 03:01:37 +0600

    Database connection with MySQL with the help of JDBC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        String url = "jdbc:mysql://localhost:3306/Student";
+        String user = "root";
+        String pass = "252646";
+
+        try(Connection connection = DriverManager.getConnection(url, user, pass)) {
+            System.out.println("Database connected Successfully!");
+        } catch (SQLException e) {
+            System.err.println("Connection Failed: " + e.getMessage());
+        }
     }
 }
